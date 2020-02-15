3f04feec84a908a5417d0a86014066d32d501979
bear2u <bear2u@naver.com>
2020-02-15 13:29:43 +0900

    StreamBuilder 기초

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,32 +29,25 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   ViewModel _viewModel = ViewModel();
 
-  int _counter = 0;
-
   void _incrementCounter() async {
-    await _viewModel.increment();
-    setState(() {});
+    _viewModel.increment();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
-            FutureBuilder<int>(
-              future: _viewModel.count,
+            StreamBuilder<int>(
+              stream: _viewModel.countStream,
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
                   return Center(
--- lib/view_model.dart
+++ lib/view_model.dart
@@ -1,18 +1,35 @@
+import 'dart:async';
+
 class ViewModel {
+  StreamController<int> _countController = StreamController<int>();
+
+  Stream<int> get countStream => _countController.stream;
+
   Service _service = new Service();
   int _count = 0;
 
   Future<int> get count =>
       Future.delayed(Duration(seconds: 3), () => Future.value(_count));
 
+  ViewModel() {
+    _countController.sink.add(_count);
+  }
+
+  Future<int> delay(int value) =>
+      Future.delayed(Duration(seconds: 3), () => Future.value(value));
+
   increment() async {
     _count = await _service.increment(_count);
-    return count;
+    _countController.sink.add(await delay(_count));
   }
 
   decrement() async {
     await _service.decrement(_count);
     return count;
+  }
+
+  dispose() {
+    _countController.close();
   }
 }
 
